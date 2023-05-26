49612d03ded890633b989d28444747bba92b5c57
Karen Kintrup <karenkintrup@gmail.com>
2023-05-26 10:37:26 +0200

    watchlist current status

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  resources :lists do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
+
+  # get "/lists", to: "lists#index"
+  # get "/lists/new", to: "lists#new"
+  # post "/lists", to: "lists#create"
+  # get "/lists/:id", to: "lists#show"
+
+  # get "lists/:id/bookmarks/new", to: "bookmarks#new"
+  # post "lists/:id/bookmarks"
+
 end
